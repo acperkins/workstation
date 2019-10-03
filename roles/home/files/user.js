8352272bb50cb9389f4fc49879633a8b79b187e3
Anthony Perkins <anthony@acperkins.com>
2019-10-03 21:11:35 +0100

    Add more Firefox preferences

--- roles/home/files/user.js
+++ roles/home/files/user.js
@@ -1,2 +1,10 @@
-user_pref("browser.startup.homepage", "about:blank");
+user_pref("browser.formfill.enable", false);
 user_pref("browser.newtabpage.enabled", false);
+user_pref("browser.search.countryCode", "GB");
+user_pref("browser.search.region", "GB");
+user_pref("browser.search.suggest.enabled", false);
+user_pref("browser.startup.homepage", "about:blank");
+user_pref("general.smoothScroll", false);
+user_pref("network.security.ports.banned.override", "6697");
+user_pref("privacy.resistFingerprinting", true);
+user_pref("signon.rememberSignons", false);
