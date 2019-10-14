33717356210745a94c75fa5744ed0f16d76099b7
Anthony Perkins <anthony@acperkins.com>
2019-10-14 10:14:40 +0100

    Always warn when closing Firefox

--- roles/home/files/user.js
+++ roles/home/files/user.js
@@ -4,6 +4,7 @@ user_pref("browser.search.countryCode", "GB");
 user_pref("browser.search.region", "GB");
 user_pref("browser.search.suggest.enabled", false);
 user_pref("browser.startup.homepage", "about:blank");
+user_pref("browser.tabs.warnOnClose", true);
 user_pref("general.smoothScroll", false);
 user_pref("network.security.ports.banned.override", "6697");
 user_pref("privacy.resistFingerprinting", true);
