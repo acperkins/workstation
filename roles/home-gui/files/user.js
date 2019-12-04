374a83d99915daee771cd358606881fbefa77692
Anthony Perkins <anthony@acperkins.com>
2019-12-04 16:44:25 +0000

    Disable WebRTC by default

--- roles/home-gui/files/user.js
+++ roles/home-gui/files/user.js
@@ -6,6 +6,7 @@ user_pref("browser.search.suggest.enabled", false);
 user_pref("browser.startup.homepage", "about:blank");
 user_pref("browser.tabs.warnOnClose", true);
 user_pref("general.smoothScroll", false);
+user_pref("media.peerconnection.enabled", false);
 user_pref("network.security.ports.banned.override", "6697");
 user_pref("privacy.resistFingerprinting", true);
 user_pref("signon.rememberSignons", false);
