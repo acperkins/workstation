0bb62e82feb9c0bb4264e63d76fcdd9554d9eac7
Anthony Perkins <anthony@acperkins.com>
2021-03-11 11:49:25 +0000

    Add GNOME keyboard options

--- roles/home-gui/files/gnome-setup.sh
+++ roles/home-gui/files/gnome-setup.sh
@@ -25,6 +25,7 @@ set_keyboard_and_language () {
 		layouts="[$us, $ca]"
 	fi
 	gsettings set org.gnome.desktop.input-sources sources "$layouts"
+	gsettings set org.gnome.desktop.input-sources xkb-options "['grp:alt_caps_toggle', 'nbsp:level3n', 'compose:menu']"
 }
 set_keyboard_and_language
 
