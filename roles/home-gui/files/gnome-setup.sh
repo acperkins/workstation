ac1b364cb6f3d99a47ba085d845739bd080abe8c
Anthony Perkins <anthony@acperkins.com>
2021-11-11 11:25:57 +0000

    Set Alt-Tab for switch-windows

--- roles/home-gui/files/gnome-setup.sh
+++ roles/home-gui/files/gnome-setup.sh
@@ -22,6 +22,10 @@ set_favorites_bar
 set_keyboard_and_language () {
 	gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'gb'), ('xkb', 'us')]"
 	gsettings set org.gnome.desktop.input-sources xkb-options "['lv3:ralt_switch', 'compose:caps', 'shift:both_capslock']"
+	gsettings set org.gnome.desktop.wm.keybindings switch-applications "['<Super>Tab']"
+	gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "['<Shift><Super>Tab']"
+	gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab']"
+	gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
 }
 set_keyboard_and_language
 
