33ce70e08930f3dde789a871203529fd02350ab6
Anthony Perkins <anthony@acperkins.com>
2022-07-29 16:00:21 +0100

    Make build-vim.sh mostly automatic

--- roles/fedora/files/build-vim.sh
+++ roles/fedora/files/build-vim.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 set -e
-dnf groupinstall -y 'Development Tools'
-dnf install -y ncurses-devel
+echo "Usage: $0 [install_dir] [vim_git_repo]"
+vim_dir=$(mktemp -d -t vim-XXXXXXXXXXXX --tmpdir=/var/tmp)
+dnf install -y @development-tools ncurses-devel
+git clone ${2:-https://github.com/vim/vim.git} $vim_dir
+pushd $vim_dir
 ./configure --prefix=${1:-/opt/vim} --disable-nls --disable-gui
+make
+make install
+popd
+rm -fr $vim_dir
