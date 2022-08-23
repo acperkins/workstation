bfb00f9d7bc8f70946d2cbbea34c13a00357c30d
Anthony Perkins <anthony@acperkins.com>
2022-08-23 15:00:39 +0100

    Load the new profile file for environment variables

--- console.sh
+++ console.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-. ./roles/home-cli/files/bashrc
+. ./roles/home-cli/files/profile
 ./admin-cli.yml -K -i ./local.ini
 ./user-cli.yml -i ./local.ini
--- desktop.sh
+++ desktop.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-. ./roles/home-cli/files/bashrc
+. ./roles/home-cli/files/profile
 ./admin-gui.yml -K -i ./local.ini
 ./user-gui.yml -i ./local.ini
