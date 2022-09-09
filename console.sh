fb04b0d82629a0151ee4261e22db4f211d23e041
Anthony Perkins <anthony@acperkins.com>
2022-09-09 19:44:09 +0100

    Don't run admin tasks by default

--- console.sh
+++ console.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 . ./roles/home-cli/files/profile
-./admin-cli.yml -K -i ./local.ini
 ./user-cli.yml -i ./local.ini
--- desktop.sh
+++ desktop.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 . ./roles/home-cli/files/profile
-./admin-gui.yml -K -i ./local.ini
 ./user-gui.yml -i ./local.ini
