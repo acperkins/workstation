83a7edb73f8dd012549daf8767acdab8733096df
Anthony Perkins <anthony@acperkins.com>
2023-05-23 14:28:14 +0100

    Reverted

--- roles/home-gui/files/test.js
+++
@@ -1,7 +0,0 @@
-// Test the keyboard.
-var WshShell = WScript.CreateObject("WScript.Shell");
-while (1) {
-    WshShell.SendKeys('{SCROLLLOCK}');
-    WshShell.SendKeys('{SCROLLLOCK}');
-    WScript.Sleep(1000 * 60 * 4);  // 1000ths of a second.
-}
