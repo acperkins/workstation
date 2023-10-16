56b940e874491ec17e085b4800026df6540bf95b
Anthony Perkins <anthony@acperkins.com>
2023-10-16 14:38:54 +0100

    File path is worked out automatically

--- roles/home-gui/files/kde-setup.sh
+++ roles/home-gui/files/kde-setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 plasma-apply-lookandfeel --apply org.kde.breezedark.desktop --resetLayout
-kwriteconfig5 --file "$XDG_CONFIG_HOME/konsolerc" --group KonsoleWindow --key RememberWindowSize --type bool false
+kwriteconfig5 --file konsolerc --group KonsoleWindow --key RememberWindowSize --type bool false
