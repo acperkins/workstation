269537c99f0d2f22dc6e4ce8aec293a5a303413b
Anthony Perkins <anthony@acperkins.com>
2023-10-17 15:31:25 +0100

    More KDE Plasma fixes

--- roles/home-gui/files/kde-setup.sh
+++ roles/home-gui/files/kde-setup.sh
@@ -6,6 +6,8 @@ plasma-apply-cursortheme breeze_cursors
 plasma-apply-desktoptheme default
 plasma-apply-wallpaperimage "$XDG_DATA_HOME/wallpaper.svg"
 kwriteconfig5 --file kdeglobals --group KDE --key SingleClick false
+kwriteconfig5 --file kglobalshortcutsrc --group konsole.desktop --key _k_friendly_name konsole
+kwriteconfig5 --file kglobalshortcutsrc --group konsole.desktop --key _launch Meta+Return,none,konsole
 kwriteconfig5 --file kwinrc --group NightColor --key Active true
 kwriteconfig5 --file kwinrc --group Desktops --key Id_1 16b2b027-cd5a-48fa-9bd5-c1f0f4f9e550
 kwriteconfig5 --file kwinrc --group Desktops --key Id_2 7afe0a12-0cf6-4953-8aed-811d5de9470d
@@ -13,6 +15,7 @@ kwriteconfig5 --file kwinrc --group Desktops --key Id_3 189f8351-0229-483b-a249-
 kwriteconfig5 --file kwinrc --group Desktops --key Id_4 9a917386-5b9b-4181-854f-ef0eb45151b3
 kwriteconfig5 --file kwinrc --group Desktops --key Number 4
 kwriteconfig5 --file kwinrc --group Desktops --key Rows 2
+kwriteconfig5 --file kwinrc --group Plugins --key wobblywindowsEnabled true
 
 # Keyboard.
 kwriteconfig5 --file kxkbrc --group Layout --key Use true
@@ -21,6 +24,7 @@ kwriteconfig5 --file kxkbrc --group Layout --key LayoutList gb,ca
 kwriteconfig5 --file kxkbrc --group Layout --key VariantList ,multix
 kwriteconfig5 --file kxkbrc --group Layout --key DisplayNames ,
 kwriteconfig5 --file kxkbrc --group Layout --key Options grp:win_space_toggle,compose:menu,lv3:ralt_switch,lv5:rctrl_switch
+kwriteconfig5 --file kxkbrc --group Layout --key ResetOldOptions true
 
 # Konsole.
 kwriteconfig5 --file konsolerc --group KonsoleWindow --key RememberWindowSize false
