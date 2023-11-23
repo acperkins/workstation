1a2fe127aaa4de154a35a349e5a4519b33b01f46
Anthony Perkins <anthony@acperkins.com>
2023-11-23 15:35:03 +0000

    Revert to ca+multix until Windows gets fr+afnor

--- roles/home-gui/files/gnome-setup.sh
+++ roles/home-gui/files/gnome-setup.sh
@@ -29,10 +29,10 @@ set_keyboard_and_language () {
 
     case "$LANG" in
     "fr_FR.UTF-8")
-        gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'fr+afnor'), ('xkb', 'gb')]"
+        gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'ca+multix'), ('xkb', 'gb')]"
         ;;
     *)
-        gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'gb'), ('xkb', 'fr+afnor')]"
+        gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'gb'), ('xkb', 'ca+multix')]"
         ;;
     esac
 
