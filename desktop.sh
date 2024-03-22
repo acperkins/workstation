53ee8c6bb7e4dad7d406ae90e2d79dd49f9c6d77
Anthony Perkins <anthony@acperkins.com>
2024-03-22 15:59:10 +0000

    Use uname for inventories

--- desktop.sh
+++ desktop.sh
@@ -1,12 +1,3 @@
 #!/bin/sh
 . ./roles/home-cli/files/profile
-if [ $(uname -s) = FreeBSD ]
-then
-	INVENTORY=./inventories/freebsd.ini
-elif [ $(uname -s) = OpenBSD ]
-then
-	INVENTORY=./inventories/openbsd.ini
-else
-	INVENTORY=./local.ini
-fi
-./user-gui.yml -i $INVENTORY
+./user-gui.yml -i ./inventories/$(uname -s).ini
